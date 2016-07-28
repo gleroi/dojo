27a6e3666f32ea985d68a2f5f7c1e86122da3dad
Guillaume Leroi <guillaume-externe.leroi@erdf.fr>
2016-07-28 16:52:24 +0200

    pacman: write map as one buffer, with color info

--- pacman/src/console.rs
+++ pacman/src/console.rs
@@ -8,20 +8,16 @@ use self::winapi::wincon::*;
 
 use std::char;
 
-#[derive(Copy, Clone)]
-pub struct Console {
+pub struct ConsoleInput {
     in_handle: HANDLE,
-    out_handle: HANDLE,
 }
 
-impl Console {
-    pub fn current() -> Console {
+impl ConsoleInput {
+    pub fn current() -> ConsoleInput {
         unsafe {
-            let out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
             let in_handle = GetStdHandle(STD_INPUT_HANDLE);
-            return Console {
+            return ConsoleInput {
                 in_handle: in_handle,
-                out_handle: out_handle,
             };
         }
     }
@@ -39,5 +35,62 @@ impl Console {
             }
         }
         return None;
+    }
+}
+
+#[derive(Copy, Clone)]
+pub struct ConsoleOutput {    
+    out_handle: HANDLE,
+}
+
+use std::os::raw::c_void;
+use std::ptr::null_mut;
+use std::convert;
+
+const FOREGROUND_WHITE: u16 = (FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE) as u16;
+
+impl ConsoleOutput {
+    pub fn current() -> ConsoleOutput {
+        unsafe {
+            let out_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+            return ConsoleOutput {
+                out_handle: out_handle,
+            };
+        }
+    }
+
+    pub fn write(&self, buffer: &[u8]) {
+        let mut written = 0;
+        unsafe {
+            let ptr_buffer = buffer as *const [u8] as *mut c_void; 
+            if WriteConsoleA(self.out_handle, ptr_buffer, buffer.len() as u32, &mut written, null_mut()) == 0 {
+                panic!("console output: could not write: {0}", GetLastError());
+            }
+        }
+    }
+
+    pub fn write_rect(&self, buffer: &[u8], width: usize) {
+        if buffer.len() == 0 || buffer.len() % width != 0 {
+            panic!("write_rect: buffer length ({}) in not divisible by {}", buffer.len(), width);
+        }
+        let len = buffer.len();
+        let mut data: Vec<CHAR_INFO> = Vec::with_capacity(len);
+
+        for character in buffer {
+            let cell = CHAR_INFO { UnicodeChar: *character as u16, Attributes: BACKGROUND_BLUE as u16 | FOREGROUND_WHITE as u16 };
+            data.push(cell);
+        }
+
+        let iwidth : i16 = width as i16;
+        let iheight = len as i16 / iwidth;
+        let ptr_data : &[CHAR_INFO] = data.as_ref();
+        let mut out_rect = &mut SMALL_RECT { Top:0, Left:0, Bottom: iheight, Right: iwidth };
+        unsafe {
+            WriteConsoleOutputA(self.out_handle, 
+                ptr_data as *const [CHAR_INFO] as *const CHAR_INFO, 
+                COORD { X: iwidth, Y: iheight },
+                COORD { X: 0, Y: 0 },
+                out_rect);
+        }
     }
 }
--- pacman/src/main.rs
+++ pacman/src/main.rs
@@ -6,8 +6,8 @@ enum Cell {
     Wall,
 }
 
-const GRID_WIDTH : usize = 25;
-const GRID_HEIGHT : usize = 25;
+const GRID_WIDTH : usize = 52;
+const GRID_HEIGHT : usize = 26;
 
 struct Grid {
     cells: [Cell; GRID_HEIGHT * GRID_WIDTH],
@@ -54,34 +54,27 @@ fn update_buffer(buffer: &mut [u8], grid: &Grid, pacman: &Pacman) {
         }
     }
 
-    let position = (pacman.position.y as usize * GRID_WIDTH + pacman.position.y as usize) as usize;
+    let position = (pacman.position.y as usize * GRID_WIDTH + pacman.position.x as usize) as usize;
     buffer[position] = match pacman.direction {
         Direction::Down => 'W' as u8,
         Direction::Left => '3' as u8,
         Direction::Up => 'V' as u8,
         Direction::Right => 'K' as u8,
     }
-
 }
 
-use std::io::Write;
-
-fn print_buffer(buffer: &[u8]) {
-    let mut out = std::io::stdout();
-    for y in 0..GRID_HEIGHT {
-        out.write_all(&buffer[y*GRID_WIDTH..(y+1) * GRID_WIDTH]);
-        out.write(&['\n' as u8; 1]);
-    }
+fn print_buffer(output: &ConsoleOutput, buffer: &[u8]) {
+    output.write_rect(buffer, GRID_WIDTH);
 }
 
-use std::io::Read;
 use std::thread;
 use std::sync::mpsc;
+use console::*;
 
 const DEFAULT_DIRECTION : Direction = Direction::Up;
 
 fn run_input_thread(tx: mpsc::Sender<Direction>) {
-    let console = Console::current();
+    let console = ConsoleInput::current();
     let mut direction = DEFAULT_DIRECTION;
     loop {
 
@@ -106,7 +99,6 @@ fn run_input_thread(tx: mpsc::Sender<Direction>) {
     }
 }
 
-use console::*;
 
 fn main() {
 
@@ -120,6 +112,7 @@ fn main() {
         direction: DEFAULT_DIRECTION,
     };
     let mut buffer = [0 as u8; GRID_WIDTH * GRID_HEIGHT];
+    let output = ConsoleOutput::current();
 
     loop {
         match rx.try_recv() {
@@ -131,7 +124,7 @@ fn main() {
         }
         clear_buffer(&mut buffer);
         update_buffer(&mut buffer, &grid, &pacman);
-        print_buffer(&buffer);
+        print_buffer(&output, &buffer);
         thread::sleep(std::time::Duration::from_millis(16));
     }
 }
