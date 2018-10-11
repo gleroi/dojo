d944610c7075b90850f8811bf2ba9e0677624c81
Guillaume Leroi <leroi.g@gmail.com>
2018-10-11 10:55:48 +0200

    elixir: redo as erlang

--- elixir/first-try.ex
+++ elixir/first-try.ex
@@ -1,41 +1,39 @@
 defmodule FirstTry do
 
 # one exchanges message with two and detect when two exits
-  def one(other, cnt) do
+  def one(other, 0) do
     Process.monitor(other)
-    send(other, {:one, cnt, self()})
-    one_rec(other, cnt+1)
-    IO.puts("one exited")
+    send(other, {:one, 0, self()})
+    one(other, 1)
   end
 
-  def one_rec(other, cnt) do
+  def one(other, cnt) do
     Process.sleep(250)
     receive do
       {name, counter} -> 
         IO.puts("one receive: #{name} @ #{counter}")
         send(other, {:one, cnt, self()})
-        one_rec(other, cnt+1)
+        one(other, cnt+1)
       msg -> 
         IO.puts("one receive: #{inspect msg} EXIT!!!")
+        IO.puts("one exited")
     end
   end
 
+  def two(10) do
+    IO.puts("two exited")
+  end
   def two(cnt) do
     Process.sleep(250)
-    unless cnt >= 10 do
-      receive do
-        {name, counter, pid} -> 
-          IO.puts("two receive: #{name} @ #{counter}")
-          send pid, {"two", cnt}
-      end
-      two(cnt+1)
+    receive do
+      {name, counter, pid} -> 
+        IO.puts("two receive: #{name} @ #{counter}")
+        send pid, {"two", cnt}
     end
+    two(cnt+1)
   end
 
 end
 
 pid2 = spawn(FirstTry, :two, [0])
 spawn(FirstTry, :one, [pid2, 0])
-
-
-
